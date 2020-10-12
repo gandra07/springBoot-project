390864c08b29901f14ba01303e6ee9e1d657e4c0
Gandra <gandravenkatesh@gmail.com>
2020-10-12 12:24:58 -0700

    CRUD support added

--- server-side-dev/app4/src/main/java/com/gtech/app4/App4Application.java
+++ server-side-dev/app4/src/main/java/com/gtech/app4/App4Application.java
@@ -5,6 +5,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
 public class App4Application implements CommandLineRunner {
 
@@ -23,8 +26,63 @@ public class App4Application implements CommandLineRunner {
         obj.setFirstName("abc");
         obj.setLastname("xyz");
 
-        personRepository.save(obj);
+        //personRepository.save(obj);
+        Person obj3 = new Person();
+        obj3.setId(1013);
+        obj3.setFirstName("abc3");
+        obj3.setLastname("xyz3");
 
+        //personRepository.save(obj);
+        Person obj2 = new Person();
+        obj2.setId(1012);
+        obj2.setFirstName("abc2");
+        obj2.setLastname("xyz2");
+
+        personRepository.saveAll(Arrays.asList(obj,obj2,obj3));
+        System.out.println("done");
+
+/*        Person p1 = personRepository.findById(1012).get();
+        System.out.println(p1.getId());
+        System.out.println(p1.getFirstName());
+        System.out.println(p1.getLastname());
+*/
+
+/*
+        List<Person> persons = (List<Person>) personRepository.findAll();
+        for(Person person: persons){
+            System.out.println(person.getId());
+            System.out.println(person.getFirstName());
+            System.out.println(person.getLastname());
+            System.out.println("---------------------");
+ */
+/*                List<Person> persons = (List<Person>) personRepository.findAllById(Arrays.asList(101,1012));
+        for(Person person: persons){
+            System.out.println(person.getId());
+            System.out.println(person.getFirstName());
+            System.out.println(person.getLastname());
+            System.out.println("---------------------");
+            */
+/*
+        personRepository.deleteById(1013);
+*/
+/*
+        personRepository.delete(obj2);
+
+*/
+
+/*
+        personRepository.deleteAll(Arrays.asList(obj, obj2));
+*/
+
+/*
+        personRepository.deleteAll();
+*/
+        Person person = personRepository.findByFirstName("abc");
+        System.out.println(person.getId());
+        System.out.println(person.getFirstName());
+        System.out.println(person.getLastname());
+
+        }
 
     }
-}
+
--- server-side-dev/app4/src/main/java/com/gtech/app4/PersonRepository.java
+++ server-side-dev/app4/src/main/java/com/gtech/app4/PersonRepository.java
@@ -2,7 +2,17 @@ package com.gtech.app4;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PersonRepository extends CrudRepository<Person, Integer> {
+
+    public  Person findByFirstName(String firstName);
+    public Person findByLastname(String lastname);
+    public List<Person> findAllByFirstName(String firstName);
+    public List<Person> findAllByLastname(String lastname);
+
+
+
 
 }
 
